3188b73ae6d6b6c69db549412b0e94e70d37552b
Nikolas Schnellbächer <khx0@posteo.net>
2018-10-19 15:07:17 +0200

    added moments

--- poisson/Poisson_Distribution.py
+++ poisson/Poisson_Distribution.py
@@ -5,7 +5,7 @@
 # ## Poisson Distribution
 # ### Nikolas Schnellbächer (last revision 2018-10-19)
 
-# In[59]:
+# In[6]:
 
 
 import numpy as np
@@ -15,10 +15,10 @@ from scipy import stats
 from scipy.stats import poisson
 
 
-# The Poisson distribution is a very common probability distribution to describe stochastic processes. Hence it is often used as a noise model for physical processes.
+# The Poisson distribution is a very common probability distribution to describe stochastic processes and a common distribution for general statisics. Hence it is often used as a noise model for physical processes.
 # We call a discrete random variable $K$ Poisson distributed, if its probability mass function (PMF) is given by
 # \begin{align}
-# p(K=k) = \dfrac{e^{-\mu} \mu^k}{k!} \, ,
+# p_{\mu}(k) = p(K=k;\, \mu) = \dfrac{\mu^k}{k!} e^{-\mu}  \, ,
 # \end{align}
 # where $\mu$ is the only shape parameter of this distribution. The random variable $K$ can take any discrete integer value $k\ge 0$.
 
@@ -33,7 +33,28 @@ from scipy.stats import poisson
 # ```
 # where $k$ is the discrete random variable, $\mu$ the shape parameter and `loc` the location parameter of the Poisson distribution.
 
-# In[60]:
+# The central feature of the Poisson distribution is the fact, that its only shape parameter $\mu$ equals its mean and its variance.
+# \begin{align}
+# E[K] \equiv \langle k\rangle = \mu
+# \end{align}
+# and
+# \begin{align}
+# \text{Var}[K] \equiv \sigma^2 \equiv \langle k^2\rangle - \langle k\rangle^2 = \mu
+# \end{align}
+# This implies that for the Poisson distribution we also have that
+# \begin{align}
+# \dfrac{\sigma}{\mu} = \dfrac{\sqrt{\langle k^2\rangle - \langle k\rangle^2}}{\langle k\rangle} = \dfrac{\sqrt{\mu}}{\mu} = \dfrac{1}{\sqrt{\mu}} \, .
+# \end{align}
+# In comparison to other well known statistical distributions it is interesting, that the Poisson distribution can be derived from a Binomial distribution $B_{n,p}(k)=B(k;\, n, p)$ in the limit of $n\rightarrow \infty$ and $p\rightarrow 0$, such that
+# $np = \text{const.} =: \mu$.
+
+# ## Moments of the Poisson distribution
+# Additionally to the mean and the variance as given above the moments of the Poisson distribution satisfy the following recursion relation
+# \begin{align}
+# \mu \dfrac{d}{d\mu} \langle k^n \rangle = \langle k^{n+1}\rangle - \mu \langle k^n \rangle
+# \end{align}
+
+# In[4]:
 
 
 def plot_pmfs(X, muVals, labels):
@@ -97,7 +118,7 @@ def plot_pmfs(X, muVals, labels):
     return None
 
 
-# In[61]:
+# In[5]:
 
 
 get_ipython().run_cell_magic('time', '', '# create Poisson distribution\nmuVals = [1.0, 5.0, 9.0]   \nxVals = np.arange(0, 30, 1)\n\nX = np.zeros((len(xVals), len(muVals) + 1))\nX[:, 0] = xVals\n\nfor i, mu in enumerate(muVals):\n\n    yVals = poisson.pmf(xVals, mu)\n    assert xVals.shape == yVals.shape, "Error: Shape assertion failed."\n    \n    X[:, i + 1] = yVals\n    \nlabels = [r\'$\\mu = 1$\',\n          r\'$\\mu = 5$\',\n          r\'$\\mu = 9$\']\n\nplot_pmfs(X, muVals, labels)')
